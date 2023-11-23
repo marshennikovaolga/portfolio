510070886be0c22719c376668e99b54d2515d757
Olga Marshennikova <marshennikova@gmail.com>
2023-11-23 15:45:37 +0300

    функция пролистывания галереи

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,28 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './Gallery.css';
 
 export default function Gallery({ images }) {
   const [currentImage, setCurrentImage] = useState(0);
+  const touchStartX = useRef(null);
+
   const nextImage = () => {
     setCurrentImage((prevIndex) => (prevIndex + 1) % images.length);
   };
+
   const previousImage = () => {
     setCurrentImage((prevIndex) =>
       prevIndex === 0 ? images.length - 1 : prevIndex - 1
     );
   };
 
+  const handleTouchStart = (e) => {
+    touchStartX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchEnd = (e) => {
+    if (touchStartX.current === null) {
+      return;
+    }
+
+    const touchEndX = e.changedTouches[0].clientX;
+    const deltaX = touchEndX - touchStartX.current;
+
+    if (deltaX > 0) {
+      previousImage();
+    } else if (deltaX < 0) {
+      nextImage();
+    }
+
+    touchStartX.current = null;
+  };
+
   return (
-    <div className="gallery">
+    <div className="gallery" onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
       <div className='gallery__buttons'>
-        <button className="gallery-button gallery-button_back" onClick={nextImage}>
+        <button className="gallery-button gallery-button_back" onClick={previousImage}>
         </button>
         <img
           className="gallery__image"
           src={images[currentImage]}
           alt={`#${currentImage + 1}`}
         />
-        <button className="gallery-button gallery-button_go" onClick={previousImage}>
+        <button className="gallery-button gallery-button_go" onClick={nextImage}>
         </button>
       </div>
     </div>
